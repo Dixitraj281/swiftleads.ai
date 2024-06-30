8324c91a121c2de540287dd7e531fbe94bd10cef
Dixit Raj <dixitraj281@gmail.com>
2024-06-30 17:18:04 +0530

    added all files

---
+++ app/home/page.js
@@ -0,0 +1,16 @@
+import HomePage from "./index";
+
+export const metadata = {
+  title: "Home - || Swiftlead.Ai",
+  description: "Leads generator for Real estates ",
+};
+
+const HomePageLayout = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+
+export default HomePageLayout;
